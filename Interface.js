4c8117c2400995b11d317105918497eefbf75ea0
giordano <angelinigiordano@gmail.com>
2022-01-27 10:05:26 +0100

    Menu interfaccia

--- Interface.js
+++ Interface.js
@@ -1,6 +1,37 @@
 const rl = require("readline-sync");
 const Web3 = require("web3"); // fa riferimento al modulo Javascript 
 
+
+var Menu = require('terminal-menu');
+var menu = Menu({ width: 29, x: 4, y: 2 });
+menu.reset();
+menu.write('SERIOUS BUSINESS TERMINAL\n');
+menu.write('-------------------------\n');
+ 
+menu.add('ADD TRANSACTION INVOICE');
+menu.add('BUSINESS INTELLIGENCE');
+menu.add('ACCOUNTS PAYABLE');
+menu.add('LEDGER BOOKINGS');
+menu.add('INDICATOR CHART METRICS');
+menu.add('BACKUP DATA TO FLOPPY DISK');
+menu.add('RESTORE FROM FLOPPY DISK');
+menu.add('EXIT');
+ 
+menu.on('select', function (label) {
+    menu.close();
+    console.log('\nSELECTED: ' + label);
+});
+
+process.stdin.pipe(menu.createStream()).pipe(process.stdout); 
+process.stdin.setRawMode(true);
+
+menu.on('close', function () {
+    process.stdin.setRawMode(false);
+    process.stdin.end();
+});
+
+return;
+
 let mycontract = require("./mycontract.js");
 let trasformatore = require("./Utenti/Trasformatore");
 
@@ -73,6 +104,7 @@ async function deploy_handler(e, transactionHash) {
       			} 
 }
 
+/*
 function receipt_handler(receipt) {
 	try {
 		contractAddress = receipt.contractAddress;
@@ -99,3 +131,4 @@ function receipt_handler(receipt) {
 	}
 
 } 
+*/
