16b7eb06eb44d3ef00b6fe62db8cc0a5dcc72c6f
Mattia Scuriatti <S1090144@studenti.univpm.it>
2022-01-27 12:59:00 +0100

    Smart contract modificato

--- CarbonFootprint/CarbonFootPrint.sol
+++ CarbonFootprint/CarbonFootPrint.sol
@@ -3,35 +3,58 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract CarbonFootPrint {
-    struct User{
-        address user_add;
-        uint16 tipo; // 0 - Admin , 1 - Trasformatore, 2 - Fornitore, 3 - Cliente
+
+     struct Product{
+        string name_Product;
+        uint32[] carbonfootprint_Product;
+        string lot_Product;
+        uint256 carbonfootprintTot_Product;
+        uint256 amount_Product;
+        uint256 residual_amount_Product;
     }
 
-   
-    mapping(address => User) public users;
-
-    /* constructor() {
-        address trasf_add= 0xed9d02e382b34818e88B88a309c7fe71E65f419d;
-        users[trasf_add] = User(trasf_add, 1);
-    } */
-
-
-    function addUser (address _user, uint16 _tipo) public {
-        users[_user] = User(_user, _tipo);
+    struct RawMaterial{
+        string name_RawMaterial;
+        string lot_RawMaterial;
+        uint256 carbonfootprint_RawMaterial;
+        uint256 amount_RawMaterial;
+        uint256 residual_amount_RawMaterial;
     }
 
-    function getUsersTipo(address _add) public view returns(uint tipo) {
-        return users[_add].tipo;
+    mapping(string => Product) private getProductsByName;
+    mapping(string => Product) private getProductsByLot;
+    mapping(string => Product) private getRawMaterialByLot;
+    mapping(string => Product) private getRawMaterialByName;
+
+    mapping(string => bool) private ExistLot_RawMaterial;
+    mapping(string => bool) private ExistLot_Product;
+
+    address supplier;
+    address transformer;
+    address customer;
+    
+    constructor(address _supplier, address _transformer, address _customer){
+        supplier = _supplier;
+        transformer = _transformer;
+        customer = _customer;
     }
 
-    function seeder(address[] memory _initadd) public {
-        users[_initadd[4]] = User(_initadd[4], 1);
-        users[_initadd[5]] = User(_initadd[5], 2);
-        users[_initadd[6]] = User(_initadd[6], 3);
-        users[_initadd[7]] = User(_initadd[7], 1);
-        users[_initadd[8]] = User(_initadd[8], 2);
-        users[_initadd[9]] = User(_initadd[9], 3);
-    }
+    function AddRawMaterial(string memory _name , string memory _lot, uint256  _carbonfootprint, uint256  _amount) public{
+        require (msg.sender == supplier , "Errore: Questa funzione deve essere chiamata solo dai fornitori ");
+        require (_amount > 0 , "Errore: hai inserito un ammontare di materia prima minore o uguale di 0");
+        require (!ExistLot_RawMaterial[_lot], "Errore: Lotto gia' esistente");
+        require (_carbonfootprint >=0 , "Errore: CarbonFootPrint deve essere maggiore o uguale a 0");
 
-}
+        RawMaterial memory material = RawMaterial({
+            name_RawMaterial: _name ,
+            lot_RawMaterial: _lot , 
+            carbonfootprint_RawMaterial: _carbonfootprint , 
+            amount_RawMaterial: _amount ,
+            residual_amount_RawMaterial: _amount });
+
+        storage getRawMaterialByLot[_lot] = material;
+        memory getRawMaterialByName[_name] = material;
+        memory ExistLot_RawMaterial[_lot] = true;
+
+
+            }
